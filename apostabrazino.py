bf7f48d01f00048c6a5a6bb6a059cdccd1e0f70f
norbak matheus <matheus.nc10@aluno.ifsc.edu.br>
2024-10-04 11:36:56 -0300

    Add files via upload

---
+++ apostabrazino.py
@@ -0,0 +1,35 @@
+import random
+import time
+import emoji
+dinheirototal = 100
+print(f"voce tem {dinheirototal} reais")
+start
+aposta
+inicio
+def aposta():
+    A = random.choice("7", "a", "b", "py")
+    B = random.choice("7", "a", "b", "py")
+    C =  random.choice("7", "a", "b", "py")
+def start():
+    ("pressione qualquer botão para começar ou t para terminar ")
+if start == "t":
+        exit()
+def inicio():
+         start ("pressione qualquer botão para começar ou t para terminar ")
+        #  if start == "t"
+         print(time.sleep(2), A)
+         print(time.sleep(2), B)
+         if A == B:
+            print(time.sleep(5), C)
+         else:
+              print(time.sleep(2), C)
+
+         if A != B and A != C:
+             print("voce nao conseguiu nenhum ponto ")
+         dinheirototal = dinheirototal - 30
+         if A == B and A != C:
+            dinheirototal = dinheirototal + 30
+         if A == B and C:
+            dinheirototal = dinheirototal + 50
+         
+    
